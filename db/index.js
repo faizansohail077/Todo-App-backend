bc5338c215e42c052214d8f5852ae3e84f9e683b
faizan <faizansohail077@gmail.com>
2024-02-21 13:05:38 +0500

    changed mongoose version

--- db/index.js
+++ db/index.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose')
 const { db } = require('../config/db')
 
-mongoose.connect("mongodb+srv://jahanzaibkhan:Tcv08XllGCboA9ZQ@cluster0.jssv9px.mongodb.net/",
-    { useUnifiedTopology: true }
-)
+mongoose.connect("mongodb+srv://jahanzaibkhan:Tcv08XllGCboA9ZQ@cluster0.jssv9px.mongodb.net/")
     .then(() => {
         console.log('database connected')
     })
     .catch(err => {
-        console.log(err.message)
+        console.log(err.message, 'db error')
     })
