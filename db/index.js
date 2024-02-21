93c138a0e03308a0580f7fc2447808f373824636
faizan <faizansohail077@gmail.com>
2024-02-21 13:01:16 +0500

    changed db url

--- db/index.js
+++ db/index.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
 const { db } = require('../config/db')
 
-mongoose.connect("mongodb+srv://jahanzaibkhan:4DT9yLK5fhSh4vOv@cluster0.mathl1t.mongodb.net/")
+mongoose.connect("mongodb+srv://jahanzaibkhan:Tcv08XllGCboA9ZQ@cluster0.jssv9px.mongodb.net/",
+    { useUnifiedTopology: true }
+)
     .then(() => {
         console.log('database connected')
     })
